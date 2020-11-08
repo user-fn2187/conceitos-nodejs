42fc4de06f7923618b764a3d9ab092cf82ad3d30
FelipeRodrigues1337 <proen_dev@hotmail.com>
2020-11-08 16:45:22 -0300

    desafio concluido

--- README.md
+++
@@ -1,4 +0,0 @@
-# conceitos-nodejs
-API "Fake" utilizando nodejs e expressjs, criada para treinar conceitos basicos. Desafio aplicado no Bootcamp GoStack! 
-
-yarn para instalar dependências.
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { v4: uuid } = require('uuid');
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
@@ -11,23 +12,63 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repo);
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repo = repositories.find(repo => repo.id === id);
+
+  if (!repo) {
+    return response.status(400).send();
+  }
+
+  repo.title = title;
+  repo.url = url;
+  repo.techs = techs;
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repoIndex >= 0) {
+    repositories.splice(repoIndex, 1);
+  } else {
+    return response.status(400).send({error : "repo not found"});
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repo = repositories.find(repo => repo.id === id)
+
+  if (!repo) {
+    return response.status(400).send();
+  }
+  repo.likes += 1
+
+  return response.json(repo);
 });
 
 module.exports = app;
